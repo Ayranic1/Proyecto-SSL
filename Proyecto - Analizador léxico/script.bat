86f75f4f586a4b6f735ac9bc99c9ad1f059d1b32
Ayrton Sab <ayrton_leo08@hotmail.com.ar>
2024-05-31 19:46:46 -0300

    primer commit

---
+++ Proyecto - Analizador léxico/script.bat
@@ -0,0 +1,6 @@
+@echo off
+C:\Users\ayrto\Documents\.ISI - UTN\2-Sintaxis y Semánticas de los Lenguajes\TPI\Proyecto - Analizador léxico\flex scanner.l
+echo "Flex compiló! :D"
+pause
+C:\Users\ayrto\Documents\.ISI - UTN\2-Sintaxis y Semánticas de los Lenguajes\TPI\Proyecto - Analizador léxico\gcc.exe lex.yy.c -o Copiled.exe
+echo "Compiló C! - archivo: Compiled.exe"
